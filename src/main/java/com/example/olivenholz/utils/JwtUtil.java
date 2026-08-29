634c73cd2ad6872c9ed429102d4ad5750f0486ea
Ayoub Kochtben <ayoubkochtbene@gmail.com>
2026-08-29 04:00:28 +0100

    use env variables

--- src/main/java/com/example/olivenholz/utils/JwtUtil.java
+++ src/main/java/com/example/olivenholz/utils/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -18,14 +19,8 @@ import java.util.function.Function;
 @Component
 
 public class JwtUtil {
-    public static final String SECRET;
-
-    static {
-        SecureRandom random = new SecureRandom();
-        byte[] bytes = new byte[64];
-        random.nextBytes(bytes);
-        SECRET = Base64.getEncoder().encodeToString(bytes);
-    }
+    @Value("${jwt.secret}")
+    private String SECRET;
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
